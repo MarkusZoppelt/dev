4fbc08c746273d9c7ca3aec7b95add581a083ae4
Markus Zoppelt <markus@zoppelt.net>
2024-09-09 11:17:50 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dev Setup Install (Ubuntu)
+
+Mostly used to set up a new VPS or local dev environment on Ubuntu.
+
+Tested on Ubuntu 24.04 LTS.
+
+    chmod +x install.sh
+    ./install.sh
---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# if running as root, create user mz and run as mz
+if [ "$EUID" -eq 0 ]; then
+    echo "Running as root, creating user mz"
+    useradd -m mz
+    usermod -aG sudo mz
+    echo "Please set a password for mz"
+    passwd mz
+    echo "Please log in as mz and run this script again"
+    exit
+fi
+
+echo "Updating and upgrading system"
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install curl git zsh -y
+
+echo "Setting zsh as default shell"
+chsh -s /bin/zsh
+
+echo "Cloning dotfiles"
+git clone https://github.com/MarkusZoppelt/dotfiles.git ~/.dotfiles
+
+echo "Installing dotfiles"
+cd ~/.dotfiles
+bash install.sh
+cd -
+
+echo "Installing Nix via determinate systems installer"
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+
+echo "Sourcing Nix"
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
+mkdir -p ~/code
+echo "Cloning nix configuration"
+git clone https://github.com/MarkusZoppelt/nix.git ~/code/nix
+
+echo "Installing from flake"
+cd ~/code/nix
+nix run nixpkgs#home-manager -- switch --flake .#Linux
+cd -
+
+echo "Installing tailscale"
+curl -fsSL https://tailscale.com/install.sh | sh
+
+echo "Starting Zsh"
+zsh
