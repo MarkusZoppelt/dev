65194f5315ebee376345fac25641a584db343796
Markus Zoppelt <markus@zoppelt.net>
2024-09-11 22:53:33 +0200

    feat: add optional gui setup

--- install.sh
+++ install.sh
@@ -71,5 +71,36 @@ if gum confirm "Do you want to setup Tailscale?"; then
     curl -fsSL https://tailscale.com/install.sh | sh
 fi
 
+if gum confirm "Is this a Desktop? (i.e. should we setup GUI options and applications?)"; then
+    gum style --foreground 110 --bold "🚀 Setting up GUI options"
+    if lspci | rg NVIDIA; then
+        gum style --foreground 110 --bold "🚀 Setting up NVIDIA shenanigans"
+        sudo apt install nvidia-vaapi-driver libnvidia-egl-wayland1 -y
+    fi
+    sudo apt install alacritty -y
+    gsettings set org.gnome.desktop.interface enable-animations false
+
+    # Use 6 fixed workspaces instead of dynamic mode
+    gsettings set org.gnome.desktop.wm.preferences num-workspaces 5
+
+    # Use alt for pinned apps
+    gsettings set org.gnome.shell.keybindings switch-to-application-1 "['<Alt>1']"
+    gsettings set org.gnome.shell.keybindings switch-to-application-2 "['<Alt>2']"
+    gsettings set org.gnome.shell.keybindings switch-to-application-3 "['<Alt>3']"
+    gsettings set org.gnome.shell.keybindings switch-to-application-4 "['<Alt>4']"
+    gsettings set org.gnome.shell.keybindings switch-to-application-5 "['<Alt>5']"
+    gsettings set org.gnome.shell.keybindings switch-to-application-6 "['<Alt>6']"
+    gsettings set org.gnome.shell.keybindings switch-to-application-7 "['<Alt>7']"
+    gsettings set org.gnome.shell.keybindings switch-to-application-8 "['<Alt>8']"
+    gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Alt>9']"
+
+    # Use super for workspaces
+    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
+fi
+
 gum style --foreground 110 --bold "Starting zsh"
 zsh
